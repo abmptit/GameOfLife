f7d62e08a0a4a0665d76baccd14b229e490d7c40
Aymen BEN MILED <abenmiled@ecovadis.com>
2019-04-12 17:30:06 +0100

    exercice

--- GameOfLife/GameOfLife.Tests/GameOfLifeTests.cs
+++ GameOfLife/GameOfLife.Tests/GameOfLifeTests.cs
@@ -12,6 +12,7 @@ namespace GameOfLife.Tests
         public void ComputeNextGeneration_DeadMatrix_ReturnsDeadMatrix()
         {
             //Arrange
+            //Row / Column
             CellState[,] matrix = new CellState[3, 3]
             {
                  { CellState.Dead, CellState.Dead, CellState.Dead},
@@ -24,10 +25,153 @@ namespace GameOfLife.Tests
             var matrixResult = game.ComputeNextGeneration();
 
             //Assert
-            foreach(var cell in matrix)
-            {
-                Assert.Equal(CellState.Dead, cell);
-            }
+            CellState[,] expectedMatrix = new CellState[3, 3]
+           {
+                 { CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead}
+           };
+            Assert.Equal(expectedMatrix, matrix);
         }
+
+        /// <summary>
+        /// Test UnderPopulation
+        /// </summary>
+        [Fact]
+        public void ComputeNextGeneration_CellWithLessThanTwoLiveNeighboor_Dead()
+        {
+            //Arrange
+            //Row/Column
+            CellState[,] matrix = new CellState[6, 6]
+            {
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Alive, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead}
+            };
+            var game = new GameOfLife(matrix);
+
+            //Act
+            var matrixResult = game.ComputeNextGeneration();
+
+            //Assert
+            var cell1 = matrixResult[1, 0];
+            Assert.Equal(CellState.Dead, cell1);
+        }
+
+        /// <summary>
+        /// Test UnderPopulation
+        /// </summary>
+        [Fact]
+        public void ComputeNextGeneration_CellWithMoreThanTwoLiveNeighboor_Alive()
+        {
+            //Arrange
+            //Row/Column
+            CellState[,] matrix = new CellState[6, 6]
+            {
+                 { CellState.Alive, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Alive, CellState.Alive, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Alive, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead}
+            };
+            var game = new GameOfLife(matrix);
+
+            //Act
+            var matrixResult = game.ComputeNextGeneration();
+
+            //Assert
+            var cell1 = matrixResult[1, 0];
+            Assert.Equal(CellState.Alive, cell1);
+
+
+        }
+        [Fact]
+        public void ComputeNextGeneration_CellWithMoreThenThreeLiveNeighboor_Dead()
+        {
+            //Arrange
+            //Row/Column
+            CellState[,] matrix = new CellState[6, 6]
+            {
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Alive, CellState.Alive, CellState.Alive},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Alive, CellState.Alive}
+            };
+            var game = new GameOfLife(matrix);
+
+            //Act
+            var matrixResult = game.ComputeNextGeneration();
+
+            //Assert
+            var cell1 = matrixResult[4, 4];
+            Assert.Equal(CellState.Dead, cell1);
+        }
+        [Fact]
+        public void ComputeNextGeneration_DeadCellWithMoreThenThreeLiveNeighboor_Alive()
+        {
+            //Arrange
+            //Row/Column
+            CellState[,] matrix = new CellState[6, 6]
+            {
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Alive, CellState.Alive, CellState.Alive},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Alive, CellState.Alive}
+            };
+            var game = new GameOfLife(matrix);
+
+            //Act
+            var matrixResult = game.ComputeNextGeneration();
+
+            //Assert
+            var cell1 = matrixResult[3, 4];
+            Assert.Equal(CellState.Alive, cell1);
+        }
+
+        [Fact]
+        public void Test()
+        {
+            //Arrange
+            //Row/Column
+            CellState[,] matrix = new CellState[6, 6]
+            {
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Alive, CellState.Alive, CellState.Alive, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead}
+            };
+            var game = new GameOfLife(matrix);
+
+            //Act
+            var matrixResult = game.ComputeNextGeneration();
+
+            //Assert
+            var cell1 = matrixResult[1, 0];
+            CellState[,] expectedmatrix = new CellState[6, 6]
+           {
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Alive, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Alive, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Alive, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead},
+                 { CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead, CellState.Dead}
+           };
+            Assert.Equal(expectedmatrix, matrix);
+
+
+        }
+
+
+
     }
 }
--- GameOfLife/GameOfLife/GameOfLife.cs
+++ GameOfLife/GameOfLife/GameOfLife.cs
@@ -1,8 +1,11 @@
-﻿namespace GameOfLife
+﻿using System.Linq;
+
+namespace GameOfLife
 {
     public class GameOfLife
     {
         private readonly CellState[,] _matrix;
+        private CellState[,] _nextGenMatrix;
 
         public GameOfLife(CellState[,] matrix)
         {
@@ -11,22 +14,123 @@
 
         public CellState[,] ComputeNextGeneration()
         {
-            return _matrix;
+            _nextGenMatrix = (CellState[,])_matrix.Clone();
+            
+            ApplyRules();
+
+            return _nextGenMatrix;
         }
+        private void ApplyRules()
+        {
+            for (int i = 0; i < _nextGenMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < _nextGenMatrix.GetLength(1); j++)
+                {
+                    int count = CountAliveCellNeighbors(i, j);
+                    if (count < 2)
+                    {
+                        _nextGenMatrix[i, j] = CellState.Dead;
+                    }
+                    if (count > 3)
+                    {
+                        _nextGenMatrix[i, j] = CellState.Dead;
+                    }
+                    if (count == 3)
+                    {
+                        _nextGenMatrix[i, j] = CellState.Alive;
+                    }
+                }
+            }
+
+        }
+
 
         private void ApplyUnderPopulation()
         {
-        }
+            for (int i = 0; i < _nextGenMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < _nextGenMatrix.GetLength(1); j++)
+                {
+                    int count = CountAliveCellNeighbors(i, j);
+                    if (count < 2)
+                    {
+                        _nextGenMatrix[i, j] = CellState.Dead;
+                    }
 
+                    
+                }
+            }
+        }
+        private int CountAliveCellNeighbors(int x, int y)
+        {
+            int count = 0;
+            if (y > 0 && _matrix[x, y - 1] == CellState.Alive)
+            {
+                count++;
+            }
+            if (x > 0 && _matrix[x - 1, y] == CellState.Alive)
+            {
+                count++;
+            }
+            if (y > 0 && x > 0 && _matrix[x - 1, y - 1] == CellState.Alive)
+            {
+                count++;
+            }
+            if (x < (_matrix.GetLength(0) - 1) && _matrix[x + 1, y] == CellState.Alive)
+            {
+                count++;
+            }
+            if (y < (_matrix.GetLength(1) - 1) && _matrix[x, y + 1] == CellState.Alive)
+            {
+                count++;
+            }
+            if (y < (_matrix.GetLength(1) - 1) && x < (_matrix.GetLength(0) - 1) && _matrix[x + 1, y + 1] == CellState.Alive)
+            {
+                count++;
+            }
+            if (y < (_matrix.GetLength(1) - 1) && x > 0 && _matrix[x - 1, y + 1] == CellState.Alive)
+            {
+                count++;
+            }
+            if (x < (_matrix.GetLength(0) - 1) && y > 0 && _matrix[x + 1, y - 1] == CellState.Alive)
+            {
+                count++;
+            }
+            return count;
+        }
         private void ApplyOverCrowding()
         {
+            for (int i = 0; i < _nextGenMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < _nextGenMatrix.GetLength(1); j++)
+                {
+                    int count = CountAliveCellNeighbors(i, j);
+                    if (count > 3)
+                    {
+                        _nextGenMatrix[i, j] = CellState.Dead;
+                    }
+                }
+            }
         }
-
+        private void ApplyOverCrowding_New()
+        {
+            for (int i = 0; i < _nextGenMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < _nextGenMatrix.GetLength(1); j++)
+                {
+                    int count = CountAliveCellNeighbors(i, j);
+                    if (count == 3)
+                    {
+                        _nextGenMatrix[i, j] = CellState.Alive;
+                    }
+                }
+            }
+        }
         private void KillAllCells()
         {
-            for (int i = 0; i < _matrix.GetLength(0); i++)
-                for (int j = 0; j < _matrix.GetLength(1); j++)
-                    _matrix[i, j] = CellState.Dead;
+            for (int i = 0; i < _nextGenMatrix.GetLength(0); i++)
+                for (int j = 0; j < _nextGenMatrix.GetLength(1); j++)
+                    _nextGenMatrix[i, j] = CellState.Dead;
         }
 
         private void MakeAliveAllCells()
